f0e25bf89d03fd1beaa473cfc330d102584134a5
Gustavo Adame Delarosa <gua222@lehigh.edu>
2019-12-15 19:38:53 -0500

    DONE 28/28 Passed Test

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,5 @@
 /*
-Passed 27/28 test
-function_call, boolean, string, math_expression, number, identifier
+Passed 28/28 test
 */
 use nom::{
   IResult,
@@ -30,7 +29,7 @@ pub enum Node {
   Identifier { value: String },
   String { value: String },
 }
-
+/**** Basic Function **************************************************************************** */
 pub fn function_call(input: &str) -> IResult<&str, Node> {
   let (input, _) = multispace0(input)?;
   let (input, result) = identifier(input)?;
@@ -42,7 +41,6 @@ pub fn function_call(input: &str) -> IResult<&str, Node> {
   let (input, args) = arguments(input)?;
   Ok((input, Node::FunctionCall{name: function_name, children: vec![args]}))
 }
-
 pub fn arguments(input: &str) -> IResult<&str, Node> {
   let (input, _) = multispace0(input)?;
   let(input, _) = tag("(")(input)?;
@@ -53,13 +51,11 @@ pub fn arguments(input: &str) -> IResult<&str, Node> {
   let (input, _) = multispace0(input)?;
   Ok((input, Node::FunctionArguments{ children: result}))
 }
-
 pub fn number(input: &str) -> IResult<&str, Node> {
   let (input, result) = digit1(input)?;                    
   let number = result.parse::<i32>().unwrap();              
   Ok((input, Node::Number{ value: number}))                 
 }
-
 pub fn string(input: &str) -> IResult<&str, Node> {  
   let (input, result) = delimited(tag("\""), many1(alt((alphanumeric1, space1))), tag("\""))(input)?;
   let mut result_string = String::new();
@@ -68,41 +64,31 @@ pub fn string(input: &str) -> IResult<&str, Node> {
 }
   Ok((input, Node::String{ value: result_string}))
 }
-
 pub fn boolean(input: &str) -> IResult<&str, Node> {
   let (input, result) = alt((tag("true"),tag("false")))(input)?;
   let bool_value = if result == "true" {true} else {false};
   Ok((input, Node::Bool{ value: bool_value})) 
 }
-
 pub fn identifier(input: &str) -> IResult<&str, Node> {
   let (input, result) = alphanumeric1(input)?;              
   Ok((input, Node::Identifier{ value: result.to_string()})) 
 }
-
 pub fn comment(input: &str) -> IResult<&str, Node> {
   let (input, result) = identifier(input)?;             
   Ok((input, Node::Identifier{value: "".to_string()}))
 }
-
-/*TODO:
- * Figure out parenthetical_expression
- * Figure out function_definition
- */
-/*********************************************************************************************** */
+/**** Main Function **************************************************************************** */
 pub fn program(input: &str) -> IResult<&str, Node> {
   let (input, result) = many1(alt((function_definition, statement, expression)))(input)?;  
   Ok((input, Node::Program{children: result}))       
 } 
-
 pub fn expression(input: &str) -> IResult<&str, Node> {
-  let (input, result) = alt((function_call, boolean, string, math_expression, number, identifier))(input)?;
+  let (input, result) = alt((function_call, boolean, math_expression, string, number, identifier))(input)?;
   Ok((input, Node::Expression{children: vec![result]}))    
 }
-/************************************************************************************** */
+/***** Math Expression ************************************************************* */
 pub fn math_expression(input: &str) -> IResult<&str, Node> { 
-  l1(input)
-   
+  l1(input)  
 }
 pub fn l1(input: &str) -> IResult<&str, Node> {
   let (input, mut head) = l2(input)?;
@@ -119,7 +105,6 @@ pub fn l1(input: &str) -> IResult<&str, Node> {
   }
   Ok((input, head))
 }
-
 pub fn l1_infix(input: &str) -> IResult<&str, Node> {
   let (input, _) = multispace0(input)?;
   let (input, op) = alt((tag("+"),tag("-")))(input)?;
@@ -150,7 +135,6 @@ pub fn l2_infix(input: &str) -> IResult<&str, Node> {
   let (input, args) = l3(input)?;
   Ok((input, Node::MathExpression{name: op.to_string(), children: vec![args]}))
 }
-
 pub fn l3(input: &str) -> IResult<&str, Node> {
   let (input, mut head) = l4(input)?;
   let (input, tail) = many0(l3_infix)(input)?;
@@ -174,17 +158,15 @@ pub fn l3_infix(input: &str) -> IResult<&str, Node> {
   Ok((input, Node::MathExpression{name: op.to_string(), children: vec![args]}))
 }
 pub fn l4(input: &str) -> IResult<&str, Node> {
-  alt((parenthetical_expression, number, identifier, function_call ))(input)
+  alt((parenthetical_expression, number, identifier, function_call))(input)
 }
-// Math expressions with parens (1 * (2 + 3))
 pub fn parenthetical_expression(input: &str) -> IResult<&str, Node> {
   let (input, _) = tag("(")(input)?;
-  let (input, result) = math_expression(input)?;
-  let (input, _) = tag("(")(input)?;
+  let (input, result) = expression(input)?;
+  let (input, _) = tag(")")(input)?;
   Ok((input, result))
 }
-
-/*** Define a statement of the form *********************************************************************** */
+/*** Statement ************************************************************************/
 pub fn statement(input: &str) -> IResult<&str, Node> {
   let (input, _) = multispace0(input)?;
   let (input, result) = alt((function_return, variable_define))(input)?;
@@ -211,8 +193,7 @@ pub fn function_return(input: &str) -> IResult<&str, Node> {
   let (input, stat) = expression(input)?; 
   Ok((input, Node::FunctionReturn{ children: vec![stat] })) 
 }
-/****************************************************************************************** */
-
+/*** Function Definition ************************************************************************/
 pub fn function_definition(input: &str) -> IResult<&str, Node> {
   let (input, _) = tag("fn")(input)?;
   let (input, _) = multispace0(input)?;
